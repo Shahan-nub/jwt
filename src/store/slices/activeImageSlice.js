0e16db7019b91658424cfbc4852cb27ce9ad6acb
Shahan-nub <shahananwer786@gmail.com>
2024-10-03 18:53:12 +0530

    bg shadow

--- src/store/slices/activeImageSlice.js
+++ src/store/slices/activeImageSlice.js
@@ -2,7 +2,36 @@ const { createSlice } = require("@reduxjs/toolkit");
 
 const activeImageSlice = createSlice({
     name:"activeImage",
-    initialState:{},
+    initialState:[{
+        "id": "jw02731001001_02105_00004_nrca1_cal.jpg",
+        "observation_id": "jw02731001001_02105_00004_nrca1",
+        "program": 2731,
+        "details": {
+            "mission": "JWST",
+            "instruments": [
+                {
+                    "instrument": "FGS"
+                },
+                {
+                    "instrument": "NIRCam"
+                },
+                {
+                    "instrument": "NIRISS"
+                },
+                {
+                    "instrument": "NIRSpec"
+                },
+                {
+                    "instrument": "MIRI"
+                }
+            ],
+            "suffix": "_cal",
+            "description": "exposure (L2b): 2D calibrated exposure average over integrations"
+        },
+        "file_type": "jpg",
+        "thumbnail": "https://jwst-api-cdn.nyc3.cdn.digitaloceanspaces.com/thumb--jw02731001001_02105_00004_nrca1_cal_thumb.jpg",
+        "location": "https://jwst-api-cdn.nyc3.cdn.digitaloceanspaces.com/jwst--jw02731001001_02105_00004_nrca1_cal.jpg"
+    }],
     reducers:{
         addActiveImage:(store,action) => {
             return  action.payload;
